abccef4e6aeb86b775756126b0921fb728e7edfb
Chase Durbin <chase.durbin.02@gmail.com>
2021-07-01 16:19:45 -0500

    added ability to turn off responses

--- Bot.py
+++ Bot.py
@@ -8,12 +8,14 @@ import os
 client = discord.Client()
 
 dad_mode = False
+responses = True
 
 commands = '`$sponge + message: returns message in sponge case\n' \
            '$lenny: returns a random lenny face\n' \
            '$meme: returns a random meme\n' \
            '$dadmode + (on/off): changes the state of dadmode\n' \
-           '(try saying "im hungry")`'
+           '(try saying "im hungry")\n' \
+           '$responses + (on/off): changes if the bot will respond\n`'
 
 
 def sponge_word(word):
@@ -37,44 +39,51 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global dad_mode
+    global responses
     if message.author == client.user:
         return
 
     msg = message.content
 
-    if msg.startswith('$sponge'):
+    if msg.startswith('$sponge') and responses:
         sponge = msg.split('$sponge ', 1)[1]
         await message.channel.send(sponge_word(sponge))
 
-    if msg.startswith('$lenny'):
+    if msg.startswith('$lenny') and responses:
         await message.channel.send(lenny.lenny())
 
-    if msg.startswith('$meme'):
+    if msg.startswith('$meme') and responses:
         await message.channel.send(embed=await pyrandmeme())
 
-    if msg.startswith('$dadmode on'):
+    if msg.startswith('$dadmode on') and responses:
         dad_mode = True
         await message.channel.send('dad mode enabled')
 
-    if msg.startswith('$dadmode off'):
+    if msg.startswith('$dadmode off') and responses:
         dad_mode = False
         await message.channel.send('dad mode disabled')
 
-    if msg.startswith('im') and dad_mode:
+    if msg.startswith('im') and dad_mode and responses:
         dad = msg.split('im ', 1)[1]
         await message.channel.send('Hi ' + dad + ', I\'m dad!')
 
-    if msg.startswith('Im') and dad_mode:
+    if msg.startswith('Im') and dad_mode and responses:
         dad = msg.split('Im ', 1)[1]
         await message.channel.send('Hi ' + dad + ', I\'m dad!')
 
-    if msg.startswith("i'm") and dad_mode:
+    if msg.startswith("i'm") and dad_mode and responses:
         dad = msg.split("i'm ", 1)[1]
         await message.channel.send('Hi ' + dad + ', I\'m dad!')
 
-    if msg.startswith("I'm") and dad_mode:
+    if msg.startswith("I'm") and dad_mode and responses:
         dad = msg.split("I'm ", 1)[1]
         await message.channel.send('Hi ' + dad + ', I\'m dad!')
+
+    if msg.startswith('$responses off'):
+        responses = False
+
+    if msg.startswith('$responses on'):
+        responses = True
 
     if msg.startswith('$help'):
         await message.channel.send(commands)
