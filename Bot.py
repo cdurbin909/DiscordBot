62075267bcbfaf344c3bd06604d0c9d3fbfb0410
Chase Durbin <chase.durbin.02@gmail.com>
2021-07-02 00:17:36 -0500

    8ball command

--- Bot.py
+++ Bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 import lenny
 from discord.ext import commands
 from pyrandmeme import *
@@ -16,6 +17,11 @@ commands = '`$sponge + message: returns message in sponge case\n' \
            '$dadmode + (on/off): changes the state of dadmode\n' \
            '(try saying "im hungry")\n' \
            '$responses + (on/off): changes if the bot will respond\n`'
+
+eightball = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+             'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again',
+             'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
+             'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 
 
 def sponge_word(word):
@@ -92,6 +98,10 @@ async def on_message(message):
 
     if msg.startswith('$responses on'):
         responses = True
+
+    if msg.startswith('$8ball') and responses:
+        question = msg.split('$8ball ', 1)[1]
+        await message.channel.send(random.choice(eightball))
 
     if msg.startswith('$help'):
         await message.channel.send(commands)
